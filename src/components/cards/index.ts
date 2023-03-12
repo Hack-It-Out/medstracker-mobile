901f48a2ff57b4e39ef9f000c4f79cda3197be00
mogakamo <mogaka.amo254@gmail.com>
2023-03-12 11:01:18 +0300

    [refactor]: updated the app to the latest rn app

--- src/components/cards/index.ts
+++
@@ -1,2 +0,0 @@
-export {default as UpcomingMedsCard} from './UpcomingMedsCard';
-export {default as DaysCard} from './DaysCard';
--- src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export {default as UpcomingMeds} from './UpcomingMeds';
-export {default as AllReminders} from './AllReminders'
