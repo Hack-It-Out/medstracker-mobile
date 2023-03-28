1af0ad1584b33e7dae3a62c3293bda3bd255e646
mogakamo <mogaka.amo254@gmail.com>
2023-03-28 10:07:18 +0300

    feat: finished with the AppBar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,11 +1,31 @@
 /* eslint-disable prettier/prettier */
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import React from 'react';
 
+import Profile from '../assets/profile.png';
+
 const AppBar = () => {
+  const username = 'John Doe';
+
   return (
-    <View>
-      <Text className="text-black">AppBar</Text>
+    <View className="p-5">
+      <View className="flex-row justify-between items-center">
+        <View className="w-[195px]">
+          <Text className="text-black font-bold text-[22px]">
+            Reminders for your Medication
+          </Text>
+          <Text className="text-gray-500 text-[12px]">
+            Welcome back, {username}
+          </Text>
+        </View>
+        <View>
+          <Image
+            source={Profile}
+            alt="Profile"
+            className="w-[55px] h-[55px] rounded-full"
+          />
+        </View>
+      </View>
     </View>
   );
 };
--- src/screens/App/Home.tsx
+++ src/screens/App/Home.tsx
@@ -1,12 +1,11 @@
 /* eslint-disable prettier/prettier */
 import {View} from 'react-native';
 import React from 'react';
-import { AppBar } from '../../components';
-
+import {AppBar} from '../../components';
 
 const HomeScreen = () => {
   return (
-    <View>
+    <View className="flex-1 bg-white">
       <AppBar />
     </View>
   );
